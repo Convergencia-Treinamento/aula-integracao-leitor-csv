f766f50ac446b0370b01fb99b40987ca2becae54
Andre <andre_luis_pessoa@hotmail.com>
2017-03-05 23:14:46 -0300

    reducao de debitos tecnicos

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -7,25 +7,24 @@ import dao.ClienteDao;
 import model.Cliente;
 
 public class ClienteController {
-	public static void main(String[] args) throws Exception {
-		String[] clientes = null;
-		String linha  = null;
-		Cliente c = new Cliente();
-		ClienteDao cliDao = new ClienteDao();
+    public static void main(String[] args) throws Exception {
+        String[] clientes = null;
+        String linha  = null;
+        Cliente c = new Cliente();
+        ClienteDao cliDao = new ClienteDao();
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\pessoa.csv")));
-		int i = 0;
-		while ((linha = reader.readLine()) != null) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\pessoa.csv")));
+        int i = 0;
+        while ((linha = reader.readLine()) != null) {
+            clientes = linha.split("[|]");
 
-			clientes = linha.split("[|]");
-
-			c.setId(Integer.valueOf(clientes[0]));
-			c.setNome(clientes[1]);
-			c.setCpfcnpj(clientes[2]);
-			cliDao.inserir(c);
-			i++;
-			System.out.println("Foram cadastradas " + i + " colunas");
-		}
-		reader.close();
-	}
+            c.setId(Integer.valueOf(clientes[0]));
+            c.setNome(clientes[1]);
+            c.setCpfcnpj(clientes[2]);
+            cliDao.inserir(c);
+            i++;
+            System.out.println("Foram cadastradas " + i + " colunas");
+        }
+        reader.close();
+    }
 }
--- src/dao/ClienteDao.java
+++ src/dao/ClienteDao.java
@@ -8,25 +8,25 @@ import model.Cliente;
 
 public class ClienteDao {
 
-	Cliente c = new Cliente();
+    Cliente c = new Cliente();
 
-	private static final String DRIVER = "org.postgresql.Driver";
-	private static final String PASSWORD = "";
-	private static final String USER = "postgres";
-	private static final String URL = "jdbc:postgresql:excel";
+    private static final String DRIVER = "org.postgresql.Driver";
+    private static final String PASSWORD = "";
+    private static final String USER = "postgres";
+    private static final String URL = "jdbc:postgresql:excel";
 
-	public void inserir(Cliente c) throws ClassNotFoundException, SQLException{
-		try (Connection con =  DriverManager.getConnection(URL, USER, PASSWORD);) {
+    public void inserir(Cliente c) throws ClassNotFoundException, SQLException{
+        try (Connection con =  DriverManager.getConnection(URL, USER, PASSWORD);) {
 
-			Class.forName(DRIVER);
-			PreparedStatement pstmt = con.prepareStatement("insert into pessoa(id, nome, cpfcnpj) values (?, ?, ?) ");
-			pstmt.setInt(1, c.getId());
-			pstmt.setString(2, c.getNome());
-			pstmt.setString(3, c.getCpfcnpj());
-			pstmt.execute();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	}
+            Class.forName(DRIVER);
+            PreparedStatement pstmt = con.prepareStatement("insert into pessoa(id, nome, cpfcnpj) values (?, ?, ?) ");
+            pstmt.setInt(1, c.getId());
+            pstmt.setString(2, c.getNome());
+            pstmt.setString(3, c.getCpfcnpj());
+            pstmt.execute();
+
+        } catch (Exception e) {
+            Logger.getLogger(ClienteDao.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
 }
